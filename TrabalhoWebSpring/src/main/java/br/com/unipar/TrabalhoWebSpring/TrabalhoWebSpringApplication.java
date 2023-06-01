4001da18e4e839fcfa800a741c1440994a77d83c
BrunoVian <bruno.vian@edu.unipar.br>
2023-06-01 20:12:06 -0300

    Criado Projeto

---
+++ TrabalhoWebSpring/src/main/java/br/com/unipar/TrabalhoWebSpring/TrabalhoWebSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.unipar.TrabalhoWebSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoWebSpringApplication.class, args);
+	}
+
+}
---
+++ TrabalhoWebSpring/src/test/java/br/com/unipar/TrabalhoWebSpring/TrabalhoWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unipar.TrabalhoWebSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
